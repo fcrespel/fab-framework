37dcdaf972c603934322a7e0cddbff84b41bd072
Fabien Crespel <fabien@crespel.net>
2011-10-19 20:10:08 +0000

    Fab_Form_Model: extends ZFDoctrine_Form_Model with better defaults and auto field names retrieval

---
+++ library/Fab/Form/Model.php
@@ -0,0 +1,41 @@
+<?php
+
+class Fab_Form_Model extends ZFDoctrine_Form_Model
+{
+    /**
+     * Which Zend_Form element types are associated with which doctrine type?
+     * @var array
+     */
+    protected $_columnTypes = array(
+        'integer' => 'text',
+        'decimal' => 'text',
+        'float' => 'text',
+        'string' => 'text',
+        'varchar' => 'text',
+        'boolean' => 'checkbox',
+        'timestamp' => 'text',
+        'time' => 'text',
+        'date' => 'text',
+        'enum' => 'select',
+        'text' => 'text',
+    );
+    
+    /**
+     * Field names listed in this array will not be shown in the form
+     * @var array
+     */
+    protected $_ignoreFields = array('created_at', 'updated_at');
+    
+    /**
+     * @param array $options Options to pass to the Zend_Form constructor
+     */
+    public function __construct($options = null)
+    {
+        if (!isset($options['fieldLabels']) &&
+           (!isset($this->_fieldLabels) || count($this->_fieldLabels) == 0) &&
+            is_callable(array($this->_model, 'getFieldLabels'))) {
+            $this->setFieldLabels(call_user_func(array($this->_model, 'getFieldLabels')));
+        }
+        parent::__construct($options);
+    }
+}
