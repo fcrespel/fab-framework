bb6c4cc8a0f2f2b69c721eaa02efc204856afba0
Fabien Crespel <fabien@crespel.net>
2012-04-30 22:14:36 +0000

    Fab_View_Helper_ModelList: - Display the filter global action even if it is the only global action. - Add filter support in the Ldap adapter.

--- library/Fab/View/Helper/ModelList/Adapter/Ldap.php
+++ library/Fab/View/Helper/ModelList/Adapter/Ldap.php
@@ -10,8 +10,38 @@ class Fab_View_Helper_ModelList_Adapter_Ldap extends Fab_View_Helper_ModelList_A
 
     public function getPaginator($query = null, $filter = null, $sortField = null, $sortDirection = 'asc')
     {
+        $filters = array();
+        
+        // Add query to filters list if specified
+        if ($query !== null) {
+            $filters[] = $query;
+        }
+        
+        // Add extra filters to filters list
+        if ($filter) {
+            if (is_string($filter)) {
+                // Textual filter
+                $filters[] = Zend_Ldap_Filter::string($filter);
+            } else if (is_array($filter)) {
+                // Add each field as a filter
+                foreach ($filter as $field => $value) {
+                    $value = (string)$value;
+                    if (strlen($value) != 0) {
+                        $filters[] = Zend_Ldap_Filter::begins($field, $value);
+                    }
+                }
+            }
+        }
+        
+        // Build the final LDAP filter
+        $ldapFilter = null;
+        if (count($filters) > 0) {
+            $ldapFilter = new Zend_Ldap_Filter_And($filters);
+        }
+        
+        // Find all LDAP nodes
         $modelName = $this->_modelName;
-        $records = $modelName::findAll($query, $sortField);
+        $records = $modelName::findAll($ldapFilter, $sortField);
         return Zend_Paginator::factory($records);
     }
 }
